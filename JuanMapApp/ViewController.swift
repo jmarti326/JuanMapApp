f743d99edc94b6f2b490e96b8bd374a18b7b7998
Juan Marti <jmarti.dev@gmail.com>
2015-08-23 22:09:32 -0400

    Initial Commit

---
+++ JuanMapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuanMapApp
+//
+//  Created by Juan Marti on 8/23/15.
+//  Copyright (c) 2015 Juan Marti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JuanMapAppTests/JuanMapAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  JuanMapAppTests.swift
+//  JuanMapAppTests
+//
+//  Created by Juan Marti on 8/23/15.
+//  Copyright (c) 2015 Juan Marti. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JuanMapAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
